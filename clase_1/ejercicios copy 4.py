4e4763f114d174482a7033827a0c11e41ee6d296
valentinaalbizu <valentinaalbizu9@gmail.com>
2025-08-21 16:55:55 -0300

    Add files via upload

---
+++ clase_1/ejercicios copy 4.py
@@ -0,0 +1,17 @@
+#ejercicios complementarios (Valentina albizu y Sofia blangetti)
+
+#ejercicio 13
+edad=18
+print(f"Mi edad es:{edad}")
+edad= edad+5 #edad+=5
+print(f"Mi edad incrementada 5 es: {edad}")
+edad= edad-10 #edad+=10
+print(f"Mi edad disminuida 10 es: {edad}")
+
+#ejercicio 14
+altura=1.75
+print(f"Mi altura es:{altura}")
+altura=altura*4 #altura*=4
+print(f"Mi altura multiplicada por 4 es: {altura}")
+altura=altura/3 #altura/=3
+print(f"Mi altura dividida por 3 es: {altura}")
---
+++ clase_1/ejercicios copy 7.py
@@ -0,0 +1,9 @@
+#ejercicios complementarios (Valentina albizu y Sofia blangetti)
+
+#ejercicio 9
+cadena=("valentina y sofia")
+print(cadena)
+
+#ejercicio 1o
+longitud=len(cadena)
+print(f"Los nombres tienen una longitud de: {longitud}")
